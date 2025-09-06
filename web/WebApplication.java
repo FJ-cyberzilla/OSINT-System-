a8aba037eef063a739740714eeebe2fd288ea9d1
Cyberzilla™ <king.aka.tyrant@gmail.com>
2025-09-06 08:34:40 +0330

    Create WebApplication.java

---
+++ web/WebApplication.java
@@ -0,0 +1,15 @@
+package com.osint.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+@EnableRetry
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
